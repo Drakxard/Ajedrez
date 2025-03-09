54f23cd1425fa0944beefd60b85da48a63cf30f3
Drakxard <94620809+Drakxard@users.noreply.github.com>
2025-03-08 23:22:28 -0300

    webots

---
+++ robot/controllers/driff_drive_controller_cpp/build/release/driff_drive_controller_cpp.d
@@ -0,0 +1,7 @@
+build/release/driff_drive_controller_cpp.o: \
+ driff_drive_controller_cpp.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/InertialUnit.hpp
